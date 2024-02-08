b1b5ec024703ba88ca27aae5aadfd3ab4492c71c
tj <tjr99051@naver.com>
2024-02-08 16:12:24 +0900

    안녕!

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)  {
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int n1 = input.nextInt();
+            int n2 = input.nextInt();
+
+        }
+    }
+}
